e088832e3660429454388929c3b88f087b99b435
Gustavo Rodrigues <41215700+Ghustavo516@users.noreply.github.com>
2022-12-15 21:44:17 -0300

    Delete ResizeFullWeb directory

--- ResizeFullWeb/ResizeFullWeb/ResizePersonalize.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class ResizePersonalize
-    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
-        ResizeFullWeb.Width = CInt(WidthLabel.Text)
-        ResizeFullWeb.Height = CInt(HeightLabel.Text)
-        Me.Close()
-    End Sub
-End Class
